8df5f62f811ba82adc2a4b50811d92a00294b015
Jheanel <brownjheanel@gmail.com>
2020-05-20 12:13:38 -0500

    Create get_dislikes_count.php

---
+++ mybook/get_dislikes_count.php
@@ -0,0 +1,16 @@
+<?php 
+
+	include('conn.php');
+
+	$post_id = $_GET['post_id'];
+
+	$query = "SELECT COUNT(*) as count FROM dislikes WHERE post_id = :post_id";
+
+
+	$stmt = $db->prepare($query);
+	$stmt->execute(['post_id' => $post_id]);
+	$count = $stmt->fetch();
+
+	echo json_encode($count, JSON_NUMERIC_CHECK);
+
+?>
